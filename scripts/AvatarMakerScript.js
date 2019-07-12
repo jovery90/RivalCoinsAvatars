9637d4b3d60029f5c664f31e9821d676b192cbd4
“Jon <jovery90@gmail.com>
2019-07-12 15:56:45 -0500

    New Project

---
+++ scripts/AvatarMakerScript.js
@@ -0,0 +1,11 @@
+var hairStyle = 1;
+var eyeStyle = 1;
+
+
+function changeHair(){
+    document.getElementById("mainHair").style.background = "url('images/hair.png') no-repeat -738px -500px";
+    document.getElementById("mainHair").style.width = "390px";
+    document.getElementById("mainHair").style.height = "184px";
+    document.getElementById("mainHair").style.top = "110px";
+    document.getElementById("mainHair").style.left = "268px";
+  }
